4fb631a40b410cf3de18a5e1d48a9a6a079244f4
Jim-Lii <343347795@tdsb.ca>
2026-02-19 15:11:42 +0000

    dzfxgfchvjbjkn

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,22 +11,48 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		char a = 'a';
+		System.out.println(a);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String word = input.nextLine();
+		char a = word.charAt(2);
+		System.out.println(a);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String word = input.nextLine();
+		char a = word.charAt(1);
+		System.out.print("The second character user entered was: ");
+		System.out.println(a);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		int num = input.nextInt();
+		System.out.print("Your number plus 1 is: ");
+		System.out.println(num + 1);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a letter: ");
+		String letter1 = input.nextLine();
+		char a = letter1.charAt(0);
+		System.out.print("Input another letter: ");
+		String letter2 = input.nextLine();
+		char b = letter2.charAt(0);
+		System.out.println(a + b);
 	}
 
 }
